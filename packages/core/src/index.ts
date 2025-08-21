1fa15c1504aeafbee98741b90bbfbfb28f60ab46
Sai Ranjit Tummalapalli <sairanjit.tummalapalli@ayanworks.com>
2025-08-21 14:54:51 +0530

    refactor: remove @credebl/ssi-mobile
    Signed-off-by: Sai Ranjit Tummalapalli <sairanjit.tummalapalli@ayanworks.com>

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,2 +1,8 @@
+import { ConsoleLogger, LogLevel } from '@credo-ts/core'
+
 export * from './MobileSDK'
 export * from './providers'
+export * from './contexts'
+export * from './hooks'
+
+export { ConsoleLogger, LogLevel }
