03423384f824369151cafbb680b08458c9aec5a9
Sai Ranjit Tummalapalli <sairanjit.tummalapalli@ayanworks.com>
2026-02-11 19:51:29 +0530

    fix: add peer dependencies
    Signed-off-by: Sai Ranjit Tummalapalli <sairanjit.tummalapalli@ayanworks.com>

--- packages/core/tsdown.config.ts
+++ packages/core/tsdown.config.ts
@@ -1,9 +1,4 @@
 import { defineConfig } from 'tsdown'
 import config from '../../tsdown.config.base'
 
-export default defineConfig(
-  config.map((entry) => ({
-    ...entry,
-    external: ['@types/react', 'csstype'],
-  }))
-)
+export default defineConfig(config)
